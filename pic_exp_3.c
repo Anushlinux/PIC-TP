9144298be62dd47fae82f13476425f396ad57380
Anushlinux <anushrutpandit@gmail.com>
2024-02-01 13:03:20 +0530

    new exp

--- pic_exp_3.c
+++ pic_exp_3.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-
-int main(){
-    int a;
-    printf("Whats the num: ");
-    scanf("%d", &a);
-
-    for (int i = 1; i <= a; i++){
-        for (int j = 1; j <= i; j++){
-            printf("%d", i);
-        }
-
-        printf("\n");
-    }
-
-    return 0;
-}
+c
