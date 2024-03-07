18270c379b0acc2951c4434a515638fd5d113f89
Anushlinux <anushrutpandit@gmail.com>
2024-03-07 13:39:47 +0530

    ascii conversion

---
+++ .vscode/ascii_conversion.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+    printf("ASCII value of %c is %d\n", c, c);
+    return 0;
+}
