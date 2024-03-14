3a4eb9a6064edc0b474b0506096b641f8ab284d6
Anushlinux <anushrutpandit@gmail.com>
2024-03-14 12:26:25 +0530

    struct

--- Untitled-2.py
+++
@@ -1,4 +0,0 @@
-def round_number(number, multiple):
-    return multiple* round(number / multiple)
-
-print(round_number(67, 5))
---
+++ strucutres.c
@@ -0,0 +1,33 @@
+//stuct function to find the name roll marks and grade of a student
+
+#include <stdio.h>
+
+
+
+struct student
+{
+    char name[50];
+    int roll;
+    float marks;
+    char grade;
+    float percentage;
+};
+
+
+int main(void)
+{
+    struct student fe1;
+    fe1.roll = 1;
+    strcpy(fe1.name, "Anushrut");
+    fe1.marks = 84;
+    fe1.grade = 'A';
+    fe1.percentage = 99.5;
+
+    printf("Name: %s\n", fe1.name);
+    printf("Roll: %d\n", fe1.roll);
+    printf("Marks: %f\n", fe1.marks);
+    printf("Percentage: %f\n", fe1.percentage);
+    printf("Grade: %c\n", fe1.grade);
+
+    return 0;
+}
